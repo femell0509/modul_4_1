42c916e55906612de6ad5c7b59903d1fb17ed645
femell0509 <kirillvah.com@gmail.com>
2024-09-09 09:03:45 +0300

    Add files via upload

---
+++ modul_4_1.py
@@ -0,0 +1,11 @@
+from true_math import divide as div_true
+from fake_math import divide as div_fake
+
+result1 = div_fake(69, 3)
+result2 = div_fake(3, 0)
+result3 = div_true(49, 7)
+result4 = div_true(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
